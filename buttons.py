84c2f967d377cef4bba970a574389f3029a03649
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-02-15 10:19:21 +1300

    initial template

---
+++ buttons.py
@@ -0,0 +1,16 @@
+# using buttons as inputs
+
+from microbit import *
+
+# loops forever
+
+    # if button A pressed, show happy face
+
+                     
+    # if button b pressed, show sad face
+
+
+
+
+
+
