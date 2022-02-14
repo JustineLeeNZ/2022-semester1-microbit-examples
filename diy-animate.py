8c5d16faa9c5b97c78d3b57f2f6b76bd51e665da
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-02-15 10:19:11 +1300

    initial template

---
+++ diy-animate.py
@@ -0,0 +1,45 @@
+# Write your code here :-)
+from microbit import *
+
+# create a diy image for each frame of animation
+boat1 = Image("05050:"
+              "05050:"
+              "05050:"
+              "99999:"
+              "09990")
+
+boat2 = Image("00000:"
+              "05050:"
+              "05050:"
+              "05050:"
+              "99999")
+
+boat3 = Image("00000:"
+              "00000:"
+              "05050:"
+              "05050:"
+              "05050")
+
+boat4 = Image("00000:"
+              "00000:"
+              "00000:"
+              "05050:"
+              "05050")
+
+boat5 = Image("00000:"
+              "00000:"
+              "00000:"
+              "00000:"
+              "05050")
+
+boat6 = Image("00000:"
+              "00000:"
+              "00000:"
+              "00000:"
+              "00000")
+
+#create list of images
+
+
+# display images as animation
+
