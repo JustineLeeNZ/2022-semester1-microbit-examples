0b73bf294329466a1e1d050a89f8ea52e1a0f024
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-02-15 13:35:45 +1300

    Added code - single buttons

--- buttons.py
+++ buttons.py
@@ -3,11 +3,15 @@
 from microbit import *
 
 # loops forever
-
+while True:
     # if button A pressed, show happy face
+    if button_a.is_pressed():
+        display.show(Image.HAPPY)
 
-                     
+
     # if button b pressed, show sad face
+    if button_a.is_pressed():
+        display.show(Image.HAPPY)
 
 
 
