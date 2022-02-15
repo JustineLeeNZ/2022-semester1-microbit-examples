dccd1ae49efc140ed7a1c609cafdd5fac7f59707
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-02-15 22:40:32 +1300

    gestures template

---
+++ gestures2.py
@@ -0,0 +1,18 @@
+# detect when microbit tilted
+
+from microbit import *
+
+# loop forever
+while True:
+    # get value of current gesture
+    gesture = accelerometer.current_gesture()
+
+    # display image based on gesture
+    #tilt logo left is left arrow (west)
+
+    # tilt logo right is right arrow (east)
+
+    # tilt logo up is up arrow (north)
+
+    # tilt logo down is down arrow (south)
+
