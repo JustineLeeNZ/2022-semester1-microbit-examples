7d13a54fe1b465bb7e66c53ddfbb8d964df734f6
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-02-15 22:35:42 +1300

    template

---
+++ gestures.py
@@ -0,0 +1,14 @@
+# detect gestures (uses accelerometer)
+
+from microbit import *
+
+# loop forever
+
+    # get value of current gesture
+
+
+    # display image based on gesture
+    #face up is HAPPY
+
+    # anything else is ANGRY
+
