a3912c890095d7f06b4736f675481c1a52611de7
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-02-17 11:41:58 +1300

    Update random_string.py

--- random_string.py
+++ random_string.py
@@ -2,10 +2,11 @@
 from microbit import *
 
 # need this if you want random numbers or values
-
+import random
 
 # create a list of animals
-
+animals = ["Cat", "Dog", "Elephant", "Drop Bear"]
 
 #choose and display random name
+display.scroll(random.choice(animals))
 
