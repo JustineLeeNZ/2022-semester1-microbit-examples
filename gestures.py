a36cfcabeb9fe92abf5698f20e18b98d63c69d1d
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-02-17 12:19:59 +1300

    Update gestures.py

--- gestures.py
+++ gestures.py
@@ -3,12 +3,21 @@
 from microbit import *
 
 # loop forever
+while True:
 
     # get value of current gesture
+    gesture = accelerometer.current_gesture()
 
 
     # display image based on gesture
     #face up is HAPPY
+    if gesture == "face up":
+        display.show(Image.HAPPY)
+
+    elif gesture == "left":
+        display.show(Image.SAD)
 
     # anything else is ANGRY
+    else:
+        display.show(Image.ANGRY)
 
