dc21ecfaee65fb31f9def7f0d2dd0cb01be3ee27
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-02-17 11:42:09 +1300

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -4,6 +4,13 @@ from microbit import *
 
 # loops forever
 while True:
+
+    # if button a and button b pressed, show EXCITED
+    if button_a.is_pressed() and button_b.is_pressed():
+        display.show(Image.ANGRY)
+        continue
+
+
     # if button A pressed, show happy face
     if button_a.is_pressed():
         display.show(Image.HAPPY)
@@ -12,6 +19,9 @@ while True:
     # if button b pressed, show sad face
     if button_b.is_pressed():
         display.show(Image.SAD)
+
+
+
 
 
 
