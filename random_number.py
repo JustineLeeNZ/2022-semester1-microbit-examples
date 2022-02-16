8abb782e91c0c2c3464f9d5c0fe4e4770f0b5a31
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-02-17 11:42:03 +1300

    Update random_number.py

--- random_number.py
+++ random_number.py
@@ -2,9 +2,11 @@
 from microbit import *
 import random
 
-# choose and store random number between 1 and 6
+while True:
+    if button_a.is_pressed():
+        # choose and store random number between 1 and 6
+        number = random.randint(1, 6)
 
-
-# display random number
-
+        # display random number
+        display.show(str(number))
 
