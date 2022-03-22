d68743d35b70199caa96218a413154ea5c655a2b
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2022-03-23 10:24:31 +1300

    Create for-loops.py

---
+++ for-loops.py
@@ -0,0 +1,15 @@
+# Write your code here :-)
+from microbit import *
+
+# for loop repeats 6 times, shows 0 to 5
+
+for x in range(6):
+    display.scroll(x)
+    sleep(250)
+
+# for loop repeats 6 times, shows 1 to 6
+for x in range(1,7):
+    display.scroll(x)
+    sleep(250)
+
+
